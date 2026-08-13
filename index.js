237236e570d3b72941e5b8b7cdfae428c41d8338
arghadipde <arghadipdey288@gmail.com>
2026-08-13 10:58:40 +0530

    update changes

--- index.js
+++ index.js
@@ -27,8 +27,12 @@ bot.on("message", async (msg) => {
 
     if (snapshot.empty) {
       bot.sendMessage(msg.chat.id,
-`Movie 🍿 Not Found 🚫 📨 Sᴇɴᴅ Mᴏᴠɪᴇ Oʀ Sᴇʀɪᴇs Nᴀᴍᴇ ᴀɴᴅ Yᴇᴀʀ Aꜱ Pᴇʀ Gᴏᴏɢʟᴇ Sᴘᴇʟʟɪɴɢ..!! 👍
-𝐓𝐡𝐞𝐧 𝐈𝐟 𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞 𝐭𝐡𝐢𝐬 𝐦𝐨𝐯𝐢𝐞 𝐢 𝐚𝐦 𝐚𝐥𝐬𝐨 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐝 🍿🍿🥰🥰 𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐌𝐚𝐢𝐧𝐭𝐚𝐢𝐧 𝐁𝐲 @arghadipde`);
+`Movie 🍿 Not Found 🚫 
+📨 Sᴇɴᴅ Mᴏᴠɪᴇ Oʀ Sᴇʀɪᴇs Nᴀᴍᴇ ᴀɴᴅ Yᴇᴀʀ Aꜱ Pᴇʀ Gᴏᴏɢʟᴇ Sᴘᴇʟʟɪɴɢ..!! 👍
+𝐓𝐡𝐞𝐧 𝐈𝐟 𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞 𝐭𝐡𝐢𝐬 𝐦𝐨𝐯𝐢𝐞 𝐢 𝐚𝐦 𝐚𝐥𝐬𝐨 𝐩𝐫𝐨𝐯𝐢𝐝𝐞𝐝 🍿🍿🥰🥰 
+𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐌𝐚𝐢𝐧𝐭𝐚𝐢𝐧 𝐁𝐲 @arghadipde
+Also Check My Website
+ARMOVIE26.ONLINE`);
       return;
     }
 
